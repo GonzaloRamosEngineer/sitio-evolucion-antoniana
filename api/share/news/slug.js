326ccb90d08233bed7b32abbc7dba635dc5471a8
Gonzalo Ramos <gonzaloandresramos@gmail.com>
2025-10-14 14:59:57 -0300

    ESTA VERSION ESTA ESTABLE

--- api/share/news/slug.js
+++ api/share/news/slug.js
@@ -1,4 +1,4 @@
-// /api/share/news/slug.js
+// /api/share/news/slug.js Funciona
 
 const isUuid = (v = '') =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(v);
