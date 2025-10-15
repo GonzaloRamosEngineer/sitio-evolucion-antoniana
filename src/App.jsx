412c9a5b2e086bf71f3611cca07d347affae53fd
Gonzalo Ramos <gonzaloandresramos@gmail.com>
2025-10-15 13:13:27 -0300

    ESTAS VERSION ESTA ESTABLE

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx
+// src/App.jsx 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from '@/hooks/useAuth';
